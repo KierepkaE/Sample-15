acb95b4a37b77633810a4e424a1acf49f8bac335
KierepkaE <kierepkaedyta@gmail.com>
2018-08-18 15:14:45 +0100

    Initial Commit

---
+++ Cookies/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cookies
+//
+//  Created by ravnensvinger on 18.08.2018.
+//  Copyright © 2018 KierepkaE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
